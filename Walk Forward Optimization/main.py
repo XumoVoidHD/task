91693779e3f96351f273956cfb777574ad4a775c
XumoVoidHD <vedanshsahay05@gmail.com>
2024-07-08 12:36:59 +0530

    Backtesting

--- Walk Forward Optimization/main.py
+++ Walk Forward Optimization/main.py
@@ -1,5 +1,7 @@
 import datetime
 import time
+
+import numpy as np
 import talib
 import pandas as pd
 from backtesting import Backtest, Strategy
@@ -56,6 +58,7 @@ def walk_forward(filename):
     data = data.rename(columns={'timestamp': 'OpenTime','open': 'Open', "high": "High", 'low': 'Low', 'close': 'Close', 'volume': 'Volume'})
     data['OpenTime'] = pd.to_datetime(data['OpenTime'], unit='ms')
     data.set_index("OpenTime", inplace=True)
+    sym = filename.split("_")[0]
 
     start_date = data.index[0]
     end_date = start_date + pd.DateOffset(months=4) - pd.Timedelta(days=1)
@@ -70,7 +73,8 @@ def walk_forward(filename):
         df_4_months = data.loc[start_date:end_date]
         stat_master.append(run_backtest(df_4_months, MACD_Strategy))
 
-    print(stat_master)
+
+    return {sym: stat_master}
 
 def run_backtest(df, strategy, cash=10000, commission=0.002):
     stat_instance = {}
@@ -85,26 +89,88 @@ def run_backtest(df, strategy, cash=10000, commission=0.002):
     validation_data = df.loc[start_1_month_date:end_date]
 
     bt_training = Backtest(training_data, strategy, cash=cash, commission=commission)
-    stats_training = bt_training.optimize(EMA_timeperiod=range(50,130, 10), fastperiod=range(4,20, 4), slowperiod=range(20,32, 4), signal_period = range(3,15,3), maximize="Return [%]")
-    optimized_params = stats_training['_strategy']
+    stats_validation = bt_training.optimize(EMA_timeperiod=range(50,130, 10), fastperiod=range(4,20, 4), slowperiod=range(20,32, 4), signal_period = range(3,15,3), maximize="Equity Final [$]")
+    optimized_params = stats_validation['_strategy']
 
     EMA_timeperiod = optimized_params.EMA_timeperiod
     fastperiod = optimized_params.fastperiod
     slowperiod = optimized_params.slowperiod
     signal_period = optimized_params.signal_period
 
-    bt_validation = Backtest(validation_data, strategy, cash=cash, commission=commission)
-    stats_validation = bt_validation.run(EMA_timeperiod=EMA_timeperiod, fastperiod=fastperiod, slowperiod=slowperiod, signal_period=signal_period)
-
     stat_instance[f"{start_date}"] = {
+        "Start (Training)": str(stats_validation['Start']),
+        "End (Training)": str(stats_validation['End']),
+        "Start (Testing)": np.nan,
+        "End (Testing)": np.nan,
+        "Duration": str(stats_validation['Duration']),
+        "Exposure Time [%]": stats_validation['Exposure Time [%]'],
+        'Equity Final [$]': stats_validation['Equity Final [$]'],
+        'Equity Peak [$]': stats_validation['Equity Peak [$]'],
+        'Return [%]': stats_validation['Return [%]'],
+        'Buy & Hold Return [%]': stats_validation['Buy & Hold Return [%]'],
+        'Return (Ann.) [%]': stats_validation['Return (Ann.) [%]'],
+        'Return (Ann.) [%]': stats_validation['Return (Ann.) [%]'],
+        'Sharpe Ratio': stats_validation['Sharpe Ratio'],
+        'Sortino Ratio': stats_validation['Sortino Ratio'],
+        'Calmar Ratio': stats_validation['Calmar Ratio'],
+        'Max. Drawdown [%]': stats_validation['Max. Drawdown [%]'],
+        'Avg. Drawdown [%]': stats_validation['Avg. Drawdown [%]'],
+        'Max. Drawdown Duration': stats_validation['Max. Drawdown Duration'],
+        'Max. Drawdown Duration': str(stats_validation['Max. Drawdown Duration']),
+        '# Trades': stats_validation['# Trades'],
+        'Win Rate [%]': stats_validation['Win Rate [%]'],
+        'Best Trade [%]': stats_validation['Best Trade [%]'],
+        'Worst Trade [%]': stats_validation['Worst Trade [%]'],
+        'Avg. Trade [%]': stats_validation['Avg. Trade [%]'],
+        'Max. Trade Duration': str(stats_validation['Max. Trade Duration']),
+        'Avg. Trade Duration': str(stats_validation['Avg. Trade Duration']),
+        'Profit Factor': str(stats_validation['Profit Factor']),
+        'Expectancy [%]': str(stats_validation['Expectancy [%]']),
+        'SQN': str(stats_validation['SQN']),
         "EMA_timeperiod": int(EMA_timeperiod),
         "fastperiod": int(optimized_params.fastperiod),
         "slowperiod": int(optimized_params.slowperiod),
         "signal_period": int(signal_period)
     }
 
-    print(stats_training)
-    print(stats_validation)
+    bt_validation = Backtest(validation_data, strategy, cash=cash, commission=commission)
+    stats_validation = bt_validation.run(EMA_timeperiod=EMA_timeperiod, fastperiod=fastperiod, slowperiod=slowperiod, signal_period=signal_period)
+
+    stat_instance[f"{start_1_month_date}"] = {
+        "Start (Training)": np.nan,
+        "End (Training)": np.nan,
+        "Start (Testing)": str(stats_validation['Start']),
+        "End (Testing)": str(stats_validation['End']),
+        "Duration": str(stats_validation['Duration']),
+        "Exposure Time [%]": stats_validation['Exposure Time [%]'],
+        'Equity Final [$]': stats_validation['Equity Final [$]'],
+        'Equity Peak [$]': stats_validation['Equity Peak [$]'],
+        'Return [%]': stats_validation['Return [%]'],
+        'Buy & Hold Return [%]': stats_validation['Buy & Hold Return [%]'],
+        'Return (Ann.) [%]': stats_validation['Return (Ann.) [%]'],
+        'Return (Ann.) [%]': stats_validation['Return (Ann.) [%]'],
+        'Sharpe Ratio': stats_validation['Sharpe Ratio'],
+        'Sortino Ratio': stats_validation['Sortino Ratio'],
+        'Calmar Ratio': stats_validation['Calmar Ratio'],
+        'Max. Drawdown [%]': stats_validation['Max. Drawdown [%]'],
+        'Avg. Drawdown [%]': stats_validation['Avg. Drawdown [%]'],
+        'Max. Drawdown Duration': stats_validation['Max. Drawdown Duration'],
+        'Max. Drawdown Duration': str(stats_validation['Max. Drawdown Duration']),
+        '# Trades': stats_validation['# Trades'],
+        'Win Rate [%]': stats_validation['Win Rate [%]'],
+        'Best Trade [%]': stats_validation['Best Trade [%]'],
+        'Worst Trade [%]': stats_validation['Worst Trade [%]'],
+        'Avg. Trade [%]': stats_validation['Avg. Trade [%]'],
+        'Max. Trade Duration': str(stats_validation['Max. Trade Duration']),
+        'Avg. Trade Duration': str(stats_validation['Avg. Trade Duration']),
+        'Profit Factor': str(stats_validation['Profit Factor']),
+        'Expectancy [%]': str(stats_validation['Expectancy [%]']),
+        'SQN': str(stats_validation['SQN']),
+        "EMA_timeperiod": int(EMA_timeperiod),
+        "fastperiod": int(optimized_params.fastperiod),
+        "slowperiod": int(optimized_params.slowperiod),
+        "signal_period": int(signal_period)
+    }
 
     return stat_instance
 
@@ -115,10 +181,10 @@ def run_backtest(df, strategy, cash=10000, commission=0.002):
     #     validation_data = data.iloc[i-warmup_bars:i+validation_bars]
     #
     #     bt_training = Backtest(training_data, strategy, cash=cash, commission=commission)
-    #     stats_training = bt_training.optimize(EMA_timeperiod=range(50,130, 10), fastperiod=range(4,20, 4), slowperiod=range(20,32, 4), signal_period = range(3,15,3), maximize="Return [%]")
+    #     stats_validation = bt_training.optimize(EMA_timeperiod=range(50,130, 10), fastperiod=range(4,20, 4), slowperiod=range(20,32, 4), signal_period = range(3,15,3), maximize="Return [%]")
     #
-    #     EMA_timeperiod = stats_training._EMA_timeperiod
-    #     fastperiod = stats_training.fastperiod
+    #     EMA_timeperiod = stats_validation._EMA_timeperiod
+    #     fastperiod = stats_validation.fastperiod
     #     slowperiod = stats_master.slowperiod
     #     signal_period = stats_master.signal_period
     #
@@ -132,11 +198,67 @@ def run_backtest(df, strategy, cash=10000, commission=0.002):
 
 
 if __name__ == "__main__":
+
+    master = []
+
     start_time = time.time()
     with Pool() as p:
-        p.map(walk_forward,os.listdir("data"))
+        master.append(p.map(walk_forward,os.listdir("data")))
+
+
+    print(master)
+    flattened_data = []
+    for sublist in master:
+        for item in sublist:
+            for stock, records in item.items():
+                for record in records:
+                    for date, values in record.items():
+                        record_dict = {
+                            'name': stock,
+                            'Start (Training)': values['Start (Training)'],
+                            'End (Training)': values['End (Training)'],
+                            'Start (Testing)': values['Start (Testing)'],
+                            'End (Testing)': values['End (Testing)'],
+                            'Duration': values['Duration'],
+                            'Exposure Time [%]': values['Exposure Time [%]'],
+                            'Equity Final [$]': values['Equity Final [$]'],
+                            'Equity Peak [$]': values['Equity Peak [$]'],
+                            'Return [%]': values['Return [%]'],
+                            'Buy & Hold Return [%]': values['Buy & Hold Return [%]'],
+                            'Return (Ann.) [%]': values['Return (Ann.) [%]'],
+                            'Sharpe Ratio': values['Sharpe Ratio'],
+                            'Sortino Ratio': values['Sortino Ratio'],
+                            'Calmar Ratio': values['Calmar Ratio'],
+                            'Max. Drawdown [%]': values['Max. Drawdown [%]'],
+                            'Avg. Drawdown [%]': values['Avg. Drawdown [%]'],
+                            'Max. Drawdown Duration': values['Max. Drawdown Duration'],
+                            'Max. Drawdown Duration': values['Max. Drawdown Duration'],
+                            '# Trades': values['# Trades'],
+                            'Win Rate [%]': values['Win Rate [%]'],
+                            'Best Trade [%]': values['Best Trade [%]'],
+                            'Worst Trade [%]': values['Worst Trade [%]'],
+                            'Avg. Trade [%]': values['Avg. Trade [%]'],
+                            'Max. Trade Duration': values['Max. Trade Duration'],
+                            'Avg. Trade Duration': values['Avg. Trade Duration'],
+                            'Profit Factor': values['Profit Factor'],
+                            'Expectancy [%]': values['Expectancy [%]'],
+                            'SQN': values['SQN'],
+                            'EMA_timeperiod': values['EMA_timeperiod'],
+                            'fastperiod': values['fastperiod'],
+                            'slowperiod': values['slowperiod'],
+                            'signal_period': values['signal_period']
+                        }
+                        flattened_data.append(record_dict)
+
+    df = pd.DataFrame(flattened_data)
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+        print(df)
+
+    excel_path = "WFO Output.xlsx"
+    df.to_excel(excel_path, index=False, sheet_name="Sheet1")
+
+    print(f"DataFrame successfully saved to {excel_path}")
+
     end_time = time.time()
-
-
     print(f"Time taken is {end_time - start_time} seconds with multi-threading")
 
